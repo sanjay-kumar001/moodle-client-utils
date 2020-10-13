854cd0c8a7fb97e133583297e52f352bd4db11a7
Sanjay Kumar <sanjay.kumar001@gmail.com>
2020-10-13 18:57:50 +0530

    Update Config.php

--- Config.php
+++ Config.php
@@ -1,7 +1,7 @@
 <?php
 
 $config = new stdClass;
-$config->server = 'http://moodle.local';
+$config->server = 'http://example.com';
 $config->token = '4d10cee0cfd3f987f93e178ec7fd1cec';
 $config->server_address = $config->server.'/webservice/rest/server.php';
 
